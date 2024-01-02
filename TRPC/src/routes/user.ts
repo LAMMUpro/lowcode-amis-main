a914f950b96db344941ff68e349ebe11b1379de2
LAMMU <chen_yongchao@126.com>
2024-01-02 17:35:22 +0800

    接口注释

--- TRPC/src/routes/user.ts
+++ TRPC/src/routes/user.ts
@@ -4,20 +4,31 @@ import { procedure, router } from '../trpc';
 
 export const userRouter = router({
   /** http://localhost:3000/userList */
-  userList: procedure.query(async () => {
-    const users = await db.user.findMany();
-    return users;
-  }),
-  userById: procedure.input(z.string()).query(async (opts) => {
-    const { input } = opts;
-    const user = await db.user.findById(input);
-    return user;
-  }),
-  /** 创建角色 */
+  userList: procedure
+    .meta({
+      description: "获取用户列表"
+    })
+    .query(async () => {
+      const users = await db.user.findMany();
+      return users;
+    }),
+  userById: procedure
+    .meta({
+      description: "通过id查找用户"
+    })
+    .input(z.string()).query(async (opts) => {
+      const { input } = opts;
+      const user = await db.user.findById(input);
+      return user;
+    }),
+  /** 创建用户 */
   userCreate: procedure
+    .meta({
+      description: "创建用户"
+    })
     .input(z.object({ 
       /** 姓名 */
-      name: z.string()
+      name: z.string().describe("姓名属性")
     }))
     .mutation(async (opts) => {
       const { input } = opts;
