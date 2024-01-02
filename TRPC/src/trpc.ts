f76b6a2605c91b411140da7406f16beda28929fe
LAMMU <chen_yongchao@126.com>
2024-01-02 23:23:08 +0800

    删除TRPC

--- TRPC/src/trpc.ts
+++
@@ -1,6 +0,0 @@
-import { initTRPC } from '@trpc/server';
-import { TRPCPanelMeta } from "trpc-panel";
-
-const trpc = initTRPC.meta<TRPCPanelMeta>().create();
-export const router = trpc.router;
-export const procedure = trpc.procedure;
