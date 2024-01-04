2e93a7caebbc8b1ebb21b798501313135aee6983
LAMMU <chen_yongchao@126.com>
2024-01-04 17:46:07 +0800

    应用示例

---
+++ Trpc/src/routes/application.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+import { prisma } from '../../prisma/prisma.service';
+import { procedure, router } from '../trpc';
+import { ApplicationZod } from '../types/zod';
+
+export const applicationRouter = router({
+  /** 获取应用列表 */
+  pageList: procedure
+    .meta({
+      description: "获取应用列表"
+    })
+    .output(z.array(ApplicationZod))
+    .query(async () => {
+      const list = await prisma.application.findMany({
+        where: {
+          isDeleted: false
+        }
+      });
+      return list;
+    }),
+});
+
+export type ApplicationRouter = typeof applicationRouter;
--- Trpc/src/routes/index.ts
+++ Trpc/src/routes/index.ts
@@ -1,11 +1,15 @@
 import { router } from "../trpc";
+import { ApplicationRouter, applicationRouter } from './application';
 import { UserRouter, userRouter } from "./user";
 
 export const appRouter = router<{
   /** 用户 */
   user: UserRouter
+  /** 应用 */
+  application: ApplicationRouter
 }>({
-  user: userRouter
+  user: userRouter,
+  application: applicationRouter,
 })
 
 export type AppRouter = typeof appRouter;
