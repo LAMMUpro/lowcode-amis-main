ca0d54cb14c7d8d6a028b38fe26fdba3645b1603
LAMMU <chen_yongchao@126.com>
2024-01-04 16:36:06 +0800

    脚手架工程

---
+++ packages/local-cli/README.md
@@ -0,0 +1,7 @@
+## 给工程配置的脚手架
+
+```tsx
+local-cli dev
+local-cli build
+local-cli generate
+```
---
+++ packages/local-cli/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+console.log('next-cli脚手架');
+console.log('nest目录:', process.cwd());
+
